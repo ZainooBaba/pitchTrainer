2a6b3fefb2e385691abc42d52ff7903ad37eed80
ZainooBaba <34255867+ZainooBaba@users.noreply.github.com>
2023-03-05 20:28:45 -0600

    Changing  IDE to xcode

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Audio } from 'expo-av';
+// import Tts from 'react-native-tts';
+// import SoundPlayer from 'react-native-sound-player'
+
 
 export default function App() {
+
+  const playSound = React.useCallback(async () => {
+    const { sound } = await Audio.Sound.createAsync(notificationSrc);
+    setSound(sound);
+    await sound.playAsync();
+  }, []);
+
+
+
+  // let audio = new Audio("/assets/test.mp3")
+
+  // const start = () => {
+  //   // audio.play()
+  // }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello word</Text>
+      <Button
+        onPress={onClick}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+    />
+    
+
+
       <StatusBar style="auto" />
     </View>
   );
+}
+
+function onClick(){
+  alert("yo")
 }
 
 const styles = StyleSheet.create({
